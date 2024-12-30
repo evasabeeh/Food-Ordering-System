654aa0b29e8a489f344163d6ff813090480c36c8
Eva Sabeeh <97843501+evasabeeh@users.noreply.github.com>
2024-12-30 18:07:22 +0530

    Update App.jsx

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -11,7 +11,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
 
-  const url = 'http://localhost:4000';              // backend url
+  const url = 'https://fooddelivery-94pb.onrender.com';              // backend url
 
   return (
     <div>
